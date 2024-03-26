76b26cab801540db105d927fd98726adccf9ed4e
Alejandro Martinez <alex1983bcn@gmail.com>
2024-03-26 01:58:46 +0100

    Initial commit
    Initial commit

---
+++ src/test/java/com/acme/conferencesystem/ConferenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.conferencesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
