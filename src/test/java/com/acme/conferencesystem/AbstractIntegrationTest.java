67234e7bcff66c4a14ec3f72774c25eaaf0470b0
Alejandro Martinez <alex1983bcn@gmail.com>
2024-04-17 02:55:43 +0200

    15-vertical-slicing-testing (#18)

--- src/test/java/com/acme/conferencesystem/AbstractIntegrationTest.java
+++ src/test/java/com/acme/conferencesystem/AbstractIntegrationTest.java
@@ -4,12 +4,10 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {}, classes = ContainerConfig.class)
 public class AbstractIntegrationTest {
 
     protected RequestSpecification requestSpecification;
