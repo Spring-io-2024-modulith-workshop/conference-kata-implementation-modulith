23b27f27eaa3083e3d2979fe7791a2d96b8742f1
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-04-18 18:09:35 +0200

    test: ticket controller test

--- src/test/java/com/acme/conferencesystem/ticket/http/TicketControllerIntegrationTest.java
+++ src/test/java/com/acme/conferencesystem/ticket/http/TicketControllerIntegrationTest.java
@@ -1,16 +1,22 @@
 package com.acme.conferencesystem.ticket.http;
 
 import com.acme.conferencesystem.AbstractIntegrationTest;
+import com.acme.conferencesystem.ContainerConfig;
 import com.acme.conferencesystem.ticket.business.Ticket;
 import com.acme.conferencesystem.ticket.business.TicketCategory;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.modulith.test.ApplicationModuleTest;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 
+@ApplicationModuleTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(ContainerConfig.class)
 class TicketControllerIntegrationTest extends AbstractIntegrationTest {
 
     @Test
