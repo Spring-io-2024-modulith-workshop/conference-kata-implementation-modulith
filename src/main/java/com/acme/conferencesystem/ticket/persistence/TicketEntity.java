456588e5ed479ed053aa47688e96c3af6d5068b2
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-04-19 18:15:03 +0200

    fix: modify ticket table name

--- src/main/java/com/acme/conferencesystem/ticket/persistence/TicketEntity.java
+++ src/main/java/com/acme/conferencesystem/ticket/persistence/TicketEntity.java
@@ -7,7 +7,7 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Table("ticket")
+@Table("tickets")
 public record TicketEntity(@Id UUID id,
                            TicketCategory category,
                            LocalDate date) {
