c26fe9ad2cc6dbf6f1c955de30d5ff9eb3856ff9
Alejandro Martînez <alex1983bcn@gmail.com>
2024-04-29 23:27:17 +0200

    style(mapstruct): Use spring constant

--- src/main/java/com/acme/conferencesystem/cfp_proposals/business/ProposalMapper.java
+++ src/main/java/com/acme/conferencesystem/cfp_proposals/business/ProposalMapper.java
@@ -2,9 +2,11 @@ package com.acme.conferencesystem.cfp_proposals.business;
 
 import com.acme.conferencesystem.cfp_proposals.persistence.ProposalEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProposalMapper {
+
     ProposalEntity proposalToEntity(Proposal proposal);
 
     Proposal entityToProposal(ProposalEntity entity);
--- src/main/java/com/acme/conferencesystem/users/business/UserMapper.java
+++ src/main/java/com/acme/conferencesystem/users/business/UserMapper.java
@@ -2,9 +2,11 @@ package com.acme.conferencesystem.users.business;
 
 import com.acme.conferencesystem.users.persistence.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
+
     UserEntity userToEntity(User user);
 
     User entityToUser(UserEntity entity);
