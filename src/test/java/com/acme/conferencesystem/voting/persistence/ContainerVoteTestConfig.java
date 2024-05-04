437d684b12ced0ed35fe580eafc8477f8b357bef
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-05-04 09:08:50 +0200

    test: remove restart scope

--- src/test/java/com/acme/conferencesystem/voting/persistence/ContainerVoteTestConfig.java
+++ src/test/java/com/acme/conferencesystem/voting/persistence/ContainerVoteTestConfig.java
@@ -1,6 +1,5 @@
 package com.acme.conferencesystem.voting.persistence;
 
-import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +11,13 @@ public class ContainerVoteTestConfig {
 
     @Bean
     @ServiceConnection
-    @RestartScope
     public PostgreSQLContainer<?> postgreSQLContainer() {
         return new PostgreSQLContainer<>("postgres:16-alpine")
-                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/1-proposals.sql"), "/docker-entrypoint-initdb.d/1-proposals.sql")
-                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/2-users.sql"), "/docker-entrypoint-initdb.d/2-users.sql")
-                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/3-tickets.sql"), "/docker-entrypoint-initdb.d/3-tickets.sql")
-                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/4-votes.sql"), "/docker-entrypoint-initdb.d/4-votes.sql")
-                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/5-vote-proposal-scenario.sql"), "/docker-entrypoint-initdb.d/5-vote-proposal-scenario.sql")
+                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/2-users.sql", 777), "/docker-entrypoint-initdb.d/2-users.sql")
+                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/3-tickets.sql", 777), "/docker-entrypoint-initdb.d/3-tickets.sql")
+                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/1-proposals.sql", 777), "/docker-entrypoint-initdb.d/1-proposals.sql")
+                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/4-votes.sql", 777), "/docker-entrypoint-initdb.d/4-votes.sql")
+                .withCopyToContainer(MountableFile.forClasspathResource("sql/schema/5-vote-proposal-scenario.sql", 777), "/docker-entrypoint-initdb.d/5-vote-proposal-scenario.sql")
                 ;
     }
 
