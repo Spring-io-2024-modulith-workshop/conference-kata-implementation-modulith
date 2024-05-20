278588df9aa46c7adf54cbda3426c026a3510355
Alejandro Martînez <alex1983bcn@gmail.com>
2024-05-20 18:40:45 +0200

    fix: fix-hikari-conneciton-pool-not-released

--- src/test/java/com/acme/conferencesystem/ContainerConfig.java
+++ src/test/java/com/acme/conferencesystem/ContainerConfig.java
@@ -1,5 +1,9 @@
 package com.acme.conferencesystem;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +12,25 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainerConfig {
 
+    //Avoids datasource recreation
     @Bean
+    @RestartScope
+    public DataSource dataSource(PostgreSQLContainer<?> postgreSQLContainer) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setJdbcUrl(postgreSQLContainer.getJdbcUrl());
+        hikariConfig.setUsername(postgreSQLContainer.getUsername());
+        hikariConfig.setPassword(postgreSQLContainer.getPassword());
+        hikariConfig.setMaximumPoolSize(1);
+        return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    //Alternative to @DynamicPropertySource
     @ServiceConnection
-    public PostgreSQLContainer<?> postgreSQLContainer() {
+    //Keep the same bean after restarts
+    @RestartScope
+    PostgreSQLContainer<?> postgreSQLContainer() {
         return new PostgreSQLContainer<>("postgres:16-alpine");
     }
 
--- src/test/java/com/acme/conferencesystem/TestApplication.java
+++ src/test/java/com/acme/conferencesystem/TestApplication.java
@@ -1,13 +1,30 @@
 package com.acme.conferencesystem;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.
                 from(Application::main)
-                .with(ContainerConfig.class)
+                .with(LocalContainerConfig.class)
                 .run(args);
     }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    public class LocalContainerConfig {
+
+        @Bean
+        @ServiceConnection
+        @RestartScope
+        PostgreSQLContainer<?> postgreSQLContainer() {
+            return new PostgreSQLContainer<>("postgres:16-alpine");
+        }
+    }
+
 }
