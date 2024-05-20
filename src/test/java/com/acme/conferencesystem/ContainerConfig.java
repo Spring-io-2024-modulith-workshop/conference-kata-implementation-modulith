4bd3769c2ea9de715096c17cbb3d885377ff0c07
Alejandro Martînez <alex1983bcn@gmail.com>
2024-05-20 19:07:29 +0200

    Get rid off container and datasource restart scope from integration tests configuraiton

--- src/test/java/com/acme/conferencesystem/ContainerConfig.java
+++ src/test/java/com/acme/conferencesystem/ContainerConfig.java
@@ -3,7 +3,6 @@ package com.acme.conferencesystem;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
-import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -12,26 +11,32 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainerConfig {
 
-    //Avoids datasource recreation
-    @Bean
-    @RestartScope
-    public DataSource dataSource(PostgreSQLContainer<?> postgreSQLContainer) {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("org.postgresql.Driver");
-        hikariConfig.setJdbcUrl(postgreSQLContainer.getJdbcUrl());
-        hikariConfig.setUsername(postgreSQLContainer.getUsername());
-        hikariConfig.setPassword(postgreSQLContainer.getPassword());
-        hikariConfig.setMaximumPoolSize(1);
-        return new HikariDataSource(hikariConfig);
-    }
+    private static PostgreSQLContainer<?> singletonPostgreSQLContainer;
+    private static DataSource singletonDataSource;
 
     @Bean
     //Alternative to @DynamicPropertySource
     @ServiceConnection
-    //Keep the same bean after restarts
-    @RestartScope
     PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>("postgres:16-alpine");
+        if (singletonPostgreSQLContainer == null) {
+            singletonPostgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine");
+        }
+        return singletonPostgreSQLContainer;
+    }
+
+    //Avoids datasource recreation
+    @Bean
+    public DataSource dataSource(PostgreSQLContainer<?> postgreSQLContainer) {
+        if (singletonDataSource == null) {
+            HikariConfig hikariConfig = new HikariConfig();
+            hikariConfig.setDriverClassName("org.postgresql.Driver");
+            hikariConfig.setJdbcUrl(postgreSQLContainer.getJdbcUrl());
+            hikariConfig.setUsername(postgreSQLContainer.getUsername());
+            hikariConfig.setPassword(postgreSQLContainer.getPassword());
+            hikariConfig.setMaximumPoolSize(1);
+            singletonDataSource = new HikariDataSource(hikariConfig);
+        }
+        return singletonDataSource;
     }
 
 }
